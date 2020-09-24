d1997b4805610efcba7331fa4bc1ea63ec3fe3dc
Matt Postiff <postiffm@umich.edu>
2020-09-23 22:25:05 -0400

    Copy distillRefs.pl into this repo; path change

--- usfmHealthReport.sh
+++ usfmHealthReport.sh
@@ -3,5 +3,5 @@
 # Assumes you are in AndroidApps\usfm\<bible>\ or similar
 ../../usfmtools/findbadverseusfm.pl *.SFM *.usfm > usfm.vreport
 grep '\\r' *.SFM *.usfm > usfm.xrefreport
-grep '\\r' *.SFM *.usfm |  ~/bibledit-desktop/linux/distillRefs.pl > usfm.distilledxrefreport
+grep '\\r' *.SFM *.usfm |  ~/bibledit/bibledit-desktop/linux/distillRefs.pl > usfm.distilledxrefreport
 grep 'Number of total xrefs' usfm.distilledxrefreport
