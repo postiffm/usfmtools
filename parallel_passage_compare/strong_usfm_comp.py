c0d65a9d395ca973fe11cf0c4126e3e2447aa0a8
Matt Postiff <postiffm@umich.edu>
2020-09-26 21:52:41 -0400

    Second version of Jon Snoeberger parallel passage comparison tool

--- parallel_passage_compare/strong_usfm_comp.py
+++ parallel_passage_compare/strong_usfm_comp.py
@@ -1,4 +1,5 @@
 import os
+import xml.etree.ElementTree as ET
 
 def load_strong_usfm(filename):
     #returns compound array where first index is chapter,
@@ -25,44 +26,44 @@ def load_strong_usfm(filename):
                     strong = word.split('"')[1]
                     verse.append([text_word,strong])
             chapter_arr.append(verse)
-            
+    book_arr.append(chapter_arr)
     return book_arr
 
 def load_nt():
     #Returns new testament.
     #Access using obj["book"][chapter][verse]
     NT = {}
-    NT["MAT"] = load_strong_usfm(folder + "46-MATgrcmt.usfm")
-    NT["MRK"] = load_strong_usfm(folder + "47-MRKgrcmt.usfm")
-    NT["LUK"] = load_strong_usfm(folder + "48-LUKgrcmt.usfm")
-    NT["JHN"] = load_strong_usfm(folder + "49-JHNgrcmt.usfm")
-    NT["ACT"] = load_strong_usfm(folder + "50-ACTgrcmt.usfm")
-    NT["ROM"] = load_strong_usfm(folder + "51-ROMgrcmt.usfm")
-    NT["1CO"] = load_strong_usfm(folder + "52-1COgrcmt.usfm")
-    NT["2CO"] = load_strong_usfm(folder + "53-2COgrcmt.usfm")
-    NT["GAL"] = load_strong_usfm(folder + "54-GALgrcmt.usfm")
-    NT["EPH"] = load_strong_usfm(folder + "55-EPHgrcmt.usfm")
-    NT["PHP"] = load_strong_usfm(folder + "56-PHPgrcmt.usfm")
-    NT["COL"] = load_strong_usfm(folder + "57-COLgrcmt.usfm")
-    NT["1TH"] = load_strong_usfm(folder + "58-1THgrcmt.usfm")
-    NT["2TH"] = load_strong_usfm(folder + "59-2THgrcmt.usfm")
-    NT["1TI"] = load_strong_usfm(folder + "60-1TIgrcmt.usfm")
-    NT["2TI"] = load_strong_usfm(folder + "61-2TIgrcmt.usfm")
-    NT["TIT"] = load_strong_usfm(folder + "62-TITgrcmt.usfm")
-    NT["PHM"] = load_strong_usfm(folder + "63-PHMgrcmt.usfm")
-    NT["HEB"] = load_strong_usfm(folder + "64-HEBgrcmt.usfm")
-    NT["JAS"] = load_strong_usfm(folder + "65-JASgrcmt.usfm")
-    NT["1PE"] = load_strong_usfm(folder + "66-1PEgrcmt.usfm")
-    NT["2PE"] = load_strong_usfm(folder + "67-2PEgrcmt.usfm")
-    NT["1JN"] = load_strong_usfm(folder + "68-1JNgrcmt.usfm")
-    NT["2JN"] = load_strong_usfm(folder + "69-2JNgrcmt.usfm")
-    NT["3JN"] = load_strong_usfm(folder + "70-3JNgrcmt.usfm")
-    NT["JUD"] = load_strong_usfm(folder + "71-JUDgrcmt.usfm")
-    NT["REV"] = load_strong_usfm(folder + "72-REVgrcmt.usfm")
+    NT["Matthew"] = load_strong_usfm(folder + "46-MATgrcmt.usfm")
+    NT["Mark"] = load_strong_usfm(folder + "47-MRKgrcmt.usfm")
+    NT["Luke"] = load_strong_usfm(folder + "48-LUKgrcmt.usfm")
+    NT["John"] = load_strong_usfm(folder + "49-JHNgrcmt.usfm")
+    NT["Acts"] = load_strong_usfm(folder + "50-ACTgrcmt.usfm")
+    NT["Romans"] = load_strong_usfm(folder + "51-ROMgrcmt.usfm")
+    NT["1 Corinthians"] = load_strong_usfm(folder + "52-1COgrcmt.usfm")
+    NT["2 Corinthians"] = load_strong_usfm(folder + "53-2COgrcmt.usfm")
+    NT["Galations"] = load_strong_usfm(folder + "54-GALgrcmt.usfm")
+    NT["Ephesians"] = load_strong_usfm(folder + "55-EPHgrcmt.usfm")
+    NT["Philippians"] = load_strong_usfm(folder + "56-PHPgrcmt.usfm")
+    NT["Colossians"] = load_strong_usfm(folder + "57-COLgrcmt.usfm")
+    NT["1 Thessalonians"] = load_strong_usfm(folder + "58-1THgrcmt.usfm")
+    NT["2 Thessalonians"] = load_strong_usfm(folder + "59-2THgrcmt.usfm")
+    NT["1 Timothy"] = load_strong_usfm(folder + "60-1TIgrcmt.usfm")
+    NT["2 Timothy"] = load_strong_usfm(folder + "61-2TIgrcmt.usfm")
+    NT["Titus"] = load_strong_usfm(folder + "62-TITgrcmt.usfm")
+    NT["Philemon"] = load_strong_usfm(folder + "63-PHMgrcmt.usfm")
+    NT["Hebrews"] = load_strong_usfm(folder + "64-HEBgrcmt.usfm")
+    NT["James"] = load_strong_usfm(folder + "65-JASgrcmt.usfm")
+    NT["1 Peter"] = load_strong_usfm(folder + "66-1PEgrcmt.usfm")
+    NT["2 Peter"] = load_strong_usfm(folder + "67-2PEgrcmt.usfm")
+    NT["1 John"] = load_strong_usfm(folder + "68-1JNgrcmt.usfm")
+    NT["2 John"] = load_strong_usfm(folder + "69-2JNgrcmt.usfm")
+    NT["3 John"] = load_strong_usfm(folder + "70-3JNgrcmt.usfm")
+    NT["Jude"] = load_strong_usfm(folder + "71-JUDgrcmt.usfm")
+    NT["Revelation"] = load_strong_usfm(folder + "72-REVgrcmt.usfm")
     return NT
 
 
-def do_comparison(bible,comp1,comp2):
+def do_comparison(comp1,comp2):
     comp_result = ""
     
     exact_open = '<span style="background-color:green">'
@@ -81,7 +82,6 @@ def do_comparison(bible,comp1,comp2):
                 root_match = True
                 if compword[0] == word[0]:
                     exact_match = True
-                    break
             elif word[0][0:4] in compword[0]:
                 sorta_match = True
         if exact_match:
@@ -101,12 +101,11 @@ def do_comparison(bible,comp1,comp2):
     return comp_result
 
 
-def do_comparisons(bible,verse1,comp1,verse2,comp2,verse3 = '',comp3=[]):
+def do_comparisons(verse1,comp1,verse2,comp2,verse3 = '',comp3=[],verse4 = '',comp4=[]):
     html = ''
     if(comp3==[]):
-        results = [do_comparison(bible,comp1,comp2),do_comparison(bible,comp2,comp1)]
-        html = '''<table style="width:100%" border="1" width="200px" height="auto">
-  <tr>
+        results = [do_comparison(comp1,comp2),do_comparison(comp2,comp1)]
+        html = '''  <tr>
     <th>{firstverse}</th>
     <th>{secondverse}</th>
   </tr>
@@ -114,11 +113,10 @@ def do_comparisons(bible,verse1,comp1,verse2,comp2,verse3 = '',comp3=[]):
     <td>{firstcomp}</td>
     <td>{secondcomp}</td>
   </tr>
-</table>'''.format(firstverse=verse1,secondverse=verse2,firstcomp=results[0],secondcomp=results[1])
-    else:
-        results =  [do_comparison(bible,comp1,comp2+comp3),do_comparison(bible,comp2,comp1+comp3),do_comparison(bible,comp3,comp2+comp1)]
-        html = '''<table style="width:100%" border="1" width="200px" height="auto">
-  <tr>
+'''.format(firstverse=verse1,secondverse=verse2,firstcomp=results[0],secondcomp=results[1])
+    elif(comp4==[]):
+        results =  [do_comparison(comp1,comp2+comp3),do_comparison(comp2,comp1+comp3),do_comparison(comp3,comp2+comp1)]
+        html = '''  <tr>
     <th>{firstverse}</th>
     <th>{secondverse}</th>
     <th>{thirdverse}</th>
@@ -128,16 +126,61 @@ def do_comparisons(bible,verse1,comp1,verse2,comp2,verse3 = '',comp3=[]):
     <td>{secondcomp}</td>
     <td>{thirdcomp}</td>
   </tr>
-</table>'''.format(firstverse=verse1,secondverse=verse2,thirdverse=verse3,firstcomp=results[0],secondcomp=results[1],thirdcomp=results[2])
+'''.format(firstverse=verse1,secondverse=verse2,thirdverse=verse3,firstcomp=results[0],secondcomp=results[1],thirdcomp=results[2])
+    else:
+        results =  [do_comparison(comp1,comp2+comp3+comp4),do_comparison(comp2,comp1+comp3+comp4),do_comparison(comp3,comp2+comp1+comp4),do_comparison(comp4,comp1+comp2+comp3)]
+        html = '''  <tr>
+    <th>{firstverse}</th>
+    <th>{secondverse}</th>
+    <th>{thirdverse}</th>
+    <th>{fourthverse}</th>
+  </tr>
+  <tr>
+    <td>{firstcomp}</td>
+    <td>{secondcomp}</td>
+    <td>{thirdcomp}</td>
+    <td>{fourthcomp}</td>
+  </tr>
+'''.format(firstverse=verse1,secondverse=verse2,thirdverse=verse3,fourthverse=verse4,firstcomp=results[0],secondcomp=results[1],thirdcomp=results[2],fourthcomp=results[3])
+    
     return html
 
-
+###MAIN
 folder = "D:\\Documents\\Strong_USFMs\\"
 bible = load_nt()
-comp1 = bible["MAT"][3][1]+bible["MAT"][3][2]
-comp2 = bible["MRK"][1][4]
-comp3 = bible["LUK"][3][3]
-html_result = do_comparisons(bible,"Matthew 3:1-2",comp1,"Mark 1:4",comp2,"Luke 3:3",comp3)
+all_parallels = ET.parse(folder + "nt-parallel-passages.xml")
+root = all_parallels.getroot()
+html = '''<html>
+<table style="width:100%" border="1" width="200px" height="auto">
+'''
+for section in root:
+    html = html + '''  <tr>
+    <th>{sectionheader}</th>
+  </tr>
+'''.format(sectionheader=section.attrib["title"])
+    print(section.attrib["title"])
+    for _set in section:
+        for reference in _set:
+            if "-" in reference.attrib["verse"]:
+                #print(reference.attrib["book"]+reference.attrib["chapter"])
+                reference.attrib["comp"] = bible[reference.attrib["book"]][int(reference.attrib["chapter"])][int(reference.attrib["verse"].split("-")[0])]
+                for i in range(int(reference.attrib["verse"].split("-")[0]),int(reference.attrib["verse"].split("-")[1])):
+                    reference.attrib["comp"] = reference.attrib["comp"] + bible[reference.attrib["book"]][int(reference.attrib["chapter"])][i+1]
+            else:
+                #print("false")
+                #print(reference.attrib["book"])
+                reference.attrib["comp"] = bible[reference.attrib["book"]][int(reference.attrib["chapter"])][int(reference.attrib["verse"])]
+            reference.attrib["print_name"] = reference.attrib["book"] + " " + reference.attrib["chapter"] + ":" + reference.attrib["verse"]
+        if len(_set) == 4:
+            html = html + do_comparisons(_set[0].attrib["print_name"],_set[0].attrib["comp"],_set[1].attrib["print_name"],_set[1].attrib["comp"],_set[2].attrib["print_name"],_set[2].attrib["comp"],_set[3].attrib["print_name"],_set[3].attrib["comp"])
+        elif len(_set) == 3:
+            html = html + do_comparisons(_set[0].attrib["print_name"],_set[0].attrib["comp"],_set[1].attrib["print_name"],_set[1].attrib["comp"],_set[2].attrib["print_name"],_set[2].attrib["comp"])
+        elif len(_set) == 2:
+            html = html + do_comparisons(_set[0].attrib["print_name"],_set[0].attrib["comp"],_set[1].attrib["print_name"],_set[1].attrib["comp"])
 
+
+html = html + '''</table>
+</html>'''
+            
 with open(folder + 'out.html', 'w', encoding="utf-8") as f:
-    f.write(html_result)
+    f.write(html)
