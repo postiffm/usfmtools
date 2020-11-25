96745f846ef40d6669987b233b087684567a9586
Matt Postiff <postiffm@umich.edu>
2020-11-25 08:00:40 -0500

    Add possible file extension for sfm files

--- usfmHealthReport.sh
+++ usfmHealthReport.sh
@@ -2,8 +2,8 @@
 
 # Assumes you are in AndroidApps\usfm\<bible>\ or similar
 ../../usfmtools/findbadverseusfm.pl *.SFM *.usfm > usfm.vreport
-grep -s '\\r' *.SFM *.usfm > usfm.xrefreport
-grep -s '\\r' *.SFM *.usfm |  ../../usfmtools/distillRefs.pl > usfm.distilledxrefreport
+grep -s '\\r' *.SFM *.usfm *.sfm > usfm.xrefreport
+grep -s '\\r' *.SFM *.usfm *.sfm |  ../../usfmtools/distillRefs.pl > usfm.distilledxrefreport
 grep 'Number of total xrefs' usfm.distilledxrefreport
 echo "Finding USFM markers...look in markers.txt"
-python3 ../../usfmtools/findMarkers.py *.SFM *.usfm > markers.txt
+python3 ../../usfmtools/findMarkers.py *.SFM *.usfm *.sfm > markers.txt
