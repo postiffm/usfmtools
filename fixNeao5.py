d996c2814916defe1c5276fa1adec007db49c924
Matt Postiff <postiffm@umich.edu>
2021-12-14 15:58:10 -0500

    Round 3 fixes for Neao

--- fixNeao5.py
+++ fixNeao5.py
@@ -11,10 +11,14 @@ import io
 # Explanation: Replace all commas-after-ton-markings with ι iota
 
 count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+# line = text to search
+# old = the text you are searching for
+# new = the text that will replace the old text
+# ruleNum is an index into the count array above, to keep track of how many times
+#          this particular search/replace is executed
 def replaceUSFM(line, old, new, ruleNum):
     global count
-    if (regex.search(old, line) != None): # Verified ok; 42 times
+    if (regex.search(old, line) != None):
         count[ruleNum] = count[ruleNum] + len(regex.findall(old, line))
         line = regex.sub(old, new, line)
 
@@ -47,7 +51,7 @@ for file in sys.argv:
         # comma-' and comma-'' tone markings. There is also the - tone marker.
         # -, seems find throughout. ,- also seems fine.
 
-        # Ran the below on 12/8/2021
+        # Round 1: ran 12/8/2021
         #line = replaceUSFM(line, r"nʋʋdh,", r"nʋʋdhι", 1)  # Verified ok; 42 times
         #line = replaceUSFM(line, r" ´, ",   r" ´ι ", 2)    # Verified ok; 16 times
         #line = replaceUSFM(line, r" ‑: ",   r" ‑Ɔ ", 3)    # Verified ok; 29 times
@@ -63,31 +67,49 @@ for file in sys.argv:
         #line = replaceUSFM(line, r"gm,", r"gmι", 12)  # Verified 46 times
         #line = replaceUSFM(line, r"Gm,", r"Gmι", 13)  # Verified 5 times
 
-        # Ran the following on 12/9/2021....about 345 places fixed
-        line = replaceUSFM(line, r"´creedh,", r"´creedhι", 14)  # Verified 15 times
-        line = replaceUSFM(line, r"´CREEDH,", r"´CREEDHƖ", 15)  # Verified 1 times
-        line = replaceUSFM(line, r"Alɛizandr,", r"Alɛizandrι", 16)  # Verified 6 times
-        line = replaceUSFM(line, r"´cʋ,", r"´cʋι", 17) # Verified 6 times
-        line = replaceUSFM(line, r"´Cʋ,", r"´Cʋι", 18) # Verified 2 times
-        line = replaceUSFM(line, r"Moiz,", r"Moizι", 19) # Verified 32 times
-        line = replaceUSFM(line, r"Ezit,", r"Ezitι", 20) # Verified 13 times
-        line = replaceUSFM(line, r"Efɛz,", r"Efɛzι", 21) # Verified 13 times
-        line = replaceUSFM(line, r"wlukιndedh,", r"wlukιndedhι", 22) # Verified 6 times
-        line = replaceUSFM(line, r"˝Nipl,", r"˝Niplι", 23) # Verified 1 time
-        line = replaceUSFM(line, r"˝nipl,", r"˝niplι", 24) # Verified 7 times
-        line = replaceUSFM(line, r"nɛmιɛ,", r"nɛmιɛι", 25) # Verified 2 times
-        line = replaceUSFM(line, r" dh,", r"dhι", 26) # Actually did 227 times; had verified 258 times in Paratext by itself with the capital version
-        line = replaceUSFM(line, r" Dh,", r"Dhι", 27) # Actually did 32 times
+        # Round 2: ran 12/9/2021....about 345 places fixed
+        #line = replaceUSFM(line, r"´creedh,", r"´creedhι", 14)  # Verified 15 times
+        #line = replaceUSFM(line, r"´CREEDH,", r"´CREEDHƖ", 15)  # Verified 1 times
+        #line = replaceUSFM(line, r"Alɛizandr,", r"Alɛizandrι", 16)  # Verified 6 times
+        #line = replaceUSFM(line, r"´cʋ,", r"´cʋι", 17) # Verified 6 times
+        #line = replaceUSFM(line, r"´Cʋ,", r"´Cʋι", 18) # Verified 2 times
+        #line = replaceUSFM(line, r"Moiz,", r"Moizι", 19) # Verified 32 times
+        #line = replaceUSFM(line, r"Ezit,", r"Ezitι", 20) # Verified 13 times
+        #line = replaceUSFM(line, r"Efɛz,", r"Efɛzι", 21) # Verified 13 times
+        #line = replaceUSFM(line, r"wlukιndedh,", r"wlukιndedhι", 22) # Verified 6 times
+        #line = replaceUSFM(line, r"˝Nipl,", r"˝Niplι", 23) # Verified 1 time
+        #line = replaceUSFM(line, r"˝nipl,", r"˝niplι", 24) # Verified 7 times
+        #line = replaceUSFM(line, r"nɛmιɛ,", r"nɛmιɛι", 25) # Verified 2 times
+        #line = replaceUSFM(line, r" dh,", r" dhι", 26) # Actually did 227 times; had verified 258 times in Paratext by itself with the capital version
+        #line = replaceUSFM(line, r" Dh,", r" Dhι", 27) # Actually did 32 times
+    # All of the above done at or before usfm/neao git hash e8bab77
 
-        #if (regex.search(r"˝,", line) != None):  # Pattern too broad
-        #    count[1] = count[1] + len(regex.findall(r"˝,", line))
-        #    line = regex.sub(r"˝,", r"˝ι", line)
-
-        # Can't be sure yet on this one because Nathan did not mark some
-        # " s," which are stand-alone s, words. Most s, are spsd to be sι instead
-        #if (regex.search(r"s,", line) != None):
-        #    count[4] = count[4] + len(regex.findall(r"s,", line))
-        #   line = regex.sub(r"s,", r"sι", line)
+    # All below done and committed at usfm/neao git hash 543ae57
+        line = replaceUSFM(line, r"Zudas,", r"Zudasι", 1) # Verified 18 times
+        line = replaceUSFM(line, r"seaZuifʋn,", r"seaZuifʋnι", 2) # Verified 39 times
+        line = replaceUSFM(line, r"seazuifʋn,", r"seaZuifʋnι", 2) # Verified 1 time; 40 with above
+        line = replaceUSFM(line, r" ‑, ", r" ‑ι ", 3) # Verified 121 times; 122 found by script?
+        line = replaceUSFM(line, r"Glɛk,", r"Glɛkι", 4) # Verified 18 times, e.g. Acts 6:1
+        line = replaceUSFM(line, r"Glek,", r"Glɛkι", 4) # Verified 1 time, total 19 with above, Acts 17:12
+        line = replaceUSFM(line, r" s, ", r" sι ", 5) # Verified 23 times
+        line = replaceUSFM(line, r"Ananias,", r"Ananiasι", 6) # Verified 10 times
+        line = replaceUSFM(line, r" An,", r" Anι", 7) # Verified 3 times
+        line = replaceUSFM(line, r" ´z, ", r" ´zι ", 8) # Verified 9 times
+        line = replaceUSFM(line, r"´Jrʋgɔɔbhʋ,", r"´Jrʋgɔɔbhʋι", 9) # Verified 1 time
+        line = replaceUSFM(line, r" gl, ", r" glι ", 10) # Verified 10 times
+        line = replaceUSFM(line, r" nyn,", r" nynι", 11)  # Verified 10 times, like Acts 5:37, but without tone mark
+        line = replaceUSFM(line, r"Elɔd,", r"Elɔdι", 12)  # Verified 20 times
+        line = replaceUSFM(line, r"Pɔns,", r"Pɔnsι", 13)  # Verified 1 time
+        line = replaceUSFM(line, r"klandh,", r"klandhι", 14)  # Verified 2 times
+        line = replaceUSFM(line, r"bhʋ,", r"bhʋι", 15)  # Verified 43 times; 42 found by this script?
+        line = replaceUSFM(line, r"Izaak,", r"Izaakι", 16)  # Verified 3 times
+        line = replaceUSFM(line, r"˝Duud,", r"˝Duudι", 17)  # Verified 2 times
+        line = replaceUSFM(line, r"˝duud,", r"˝duudι", 18)  # Verified 2 times
+        line = replaceUSFM(line, r"Paatɛs,", r"Paatɛsι", 19)  # Verified 1 time ???
+        line = replaceUSFM(line, r"Silɛn,", r"Silɛnι", 20)  # Verified 5 times
+        line = replaceUSFM(line, r"gbl,", r"gblι", 21)  # Verified 5 times
+        line = replaceUSFM(line, r" D,", r" Dι", 22)  # Verified 6 times, all at start of sentence. 7 found by script is correct; Paratext missed one
+        line = replaceUSFM(line, r"Klɛt,", r"Klɛtι", 23)  # Verified 3 times
 
         fo.write(line)
 
