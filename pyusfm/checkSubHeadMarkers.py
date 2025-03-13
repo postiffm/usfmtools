ee5b2e634366e6860daa8b82bac2248057c8ccf3
Matt Postiff <postiffm@umich.edu>
2025-03-13 08:42:15 -0400

    New tool to swap \c and \s if needed

---
+++ pyusfm/checkSubHeadMarkers.py
@@ -0,0 +1,38 @@
+import usfm;
+import click
+import os.path
+
+# This uses the usfm.py module. 
+# Run through all .SFM files we are given, and find out if 
+# there are sequences like this:
+# \s1
+# \c
+# because they need to be swapped to be correct. You cannot have
+# a \s# header at the END of a chapter. It must be at the top of
+# the next chapter.
+
+@click.command()
+@click.argument('files', nargs=-1)
+def main(files):
+    if (len(files) == 0):
+        # __file__
+        print(f"Usage: python3 {os.path.basename(__file__)} *.SFM")
+        exit(1)
+
+    bibl = usfm.Bible("TestBible")
+
+    for filename in files:
+        if (os.path.exists(filename)):
+            bibl.loadBook(filename)
+            # Rename file to backup
+            os.rename(filename, filename+".bak")
+            bk = bibl.lastBook()
+            bk.fixSubHeads()
+            f = open(filename, "w")
+            bk.print(f)
+            f.close()
+        else:
+            print(f"ERROR: Cannot find {filename}")
+
+if __name__ == '__main__':
+    main()
