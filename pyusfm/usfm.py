f6e827acda69903ec411666482219480653c7917
Matt Postiff <postiffm@umich.edu>
2025-03-13 08:42:03 -0400

    Print more flexible; new function to fix \c \s order

--- pyusfm/usfm.py
+++ pyusfm/usfm.py
@@ -4,6 +4,7 @@
 
 import regex
 import json
+import sys
 
 # Class that handles generic USFM "stuff"
 class Usfm:
@@ -17,14 +18,15 @@ class Usfm:
         self.arg = Arg
         self.content = Content
 
-    def print(self):
+    def print(self, f=sys.stdout):
         # I have to re-constitute the content
+        # This prints each part on a separate line which is incorrect
         if (self.marker != ""):
-            print(f'{self.marker}')
+            print(f'{self.marker}', file=f)
         if (self.arg != ""):
-            print(f'{self.arg}')
+            print(f'{self.arg}', file=f)
         if (self.content != ""):
-            print(f'{self.content}')
+            print(f'{self.content}', file=f)
 
     def append(self, usfmObject):
         #print("ADDING CONTENT ", end="")
@@ -50,18 +52,19 @@ class UsfmC(Usfm):
     def __init__(self, Marker, Number, ):
         super().__init__(Marker, Number, "")
         self.arg = Number # \c X, arg X is the chapter number
-    def print(self):
-        print(f'{self.marker} {self.arg}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.arg}', file=f)
 
 class UsfmP(Usfm):
     # \p (paragraph marker)
+    # Followed immediately by a space and paragraph text, or by a new line and a verse marker.
     def __init__(self, Marker="p", Content=""):
         super().__init__(Marker, "", Content)
-    def print(self):
-        print(f'{self.marker}')
+    def print(self, f=sys.stdout):
         if (self.content != ""):
-            print(f'{self.content}')
-            # Not really sure why a \p line should have anything else on it...
+            print(f'{self.marker} {self.content}', file=f)
+        else:
+            print(f'{self.marker}', file=f)
 
 class UsfmV(Usfm):
     # \v # verse text
@@ -69,51 +72,51 @@ class UsfmV(Usfm):
         super().__init__(Marker, Number, Text)
         self.arg = Number # \v X, where X is the verse number
         self.content = Text
-    def print(self):
-        print(f'{self.marker} {self.arg} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.arg} {self.content}', file=f)
 
 class UsfmS(Usfm):
     # \s# Heading
     def __init__(self, Marker, Heading):
         super().__init__(Marker, "", Heading)
-    def print(self):
-        print(f'{self.marker} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
 
 class UsfmH(Usfm):
     # \h Heading
     def __init__(self, Marker, Content):
         super().__init__(Marker, "", Content)
-    def print(self):
-        print(f'{self.marker} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
 
 class UsfmTOC(Usfm):
     # \toc# content
     def __init__(self, Marker, Content):
         super().__init__(Marker, "", Content)
-    def print(self):
-        print(f'{self.marker} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
 
 class UsfmMT(Usfm):
     # \mt# content
     def __init__(self, Marker, Content):
         super().__init__(Marker, "", Content)
-    def print(self):
-        print(f'{self.marker} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
 
 class UsfmId(Usfm):
     # \id MAT <other text may appear here>
     def __init__(self, Marker, Id, Content):
         super().__init__(Marker, Id, Content)
         self.id = Id
-    def print(self):
-        print(f'{self.marker} {self.id} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.id} {self.content}', file=f)
 
 class UsfmM(Usfm):
     # \m text
     def __init__(self, Marker, Content):
         super().__init__(Marker, "", Content)
-    def print(self):
-        print(f'{self.marker} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
 
 class UsfmR(Usfm):
     # \r text like 
@@ -122,15 +125,39 @@ class UsfmR(Usfm):
     # \r (Mt 3,11-12; Mr 1,7-8; Nk 3,15-17) (after a \s1 line)
     def __init__(self, Marker, Content):
         super().__init__(Marker, "", Content)
-    def print(self):
-        print(f'{self.marker} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
+
+class UsfmRq(Usfm):
+    # \rq text like 
+    # \rq Ezayii 66:24 \rq*
+    # This is I think my first marker with a start and end marker
+    def __init__(self, Marker, Content):
+        super().__init__(Marker, "", Content)
+        self.endmarker="\\rq*"
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content} {self.endmarker}', file=f)
 
 class UsfmQ(Usfm):
     # \q1 text
     def __init__(self, Marker, Content):
         super().__init__(Marker, "", Content)
-    def print(self):
-        print(f'{self.marker} {self.content}')
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
+
+class UsfmLi(Usfm):
+    # \li1 text
+    def __init__(self, Marker, Content):
+        super().__init__(Marker, "", Content)
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
+
+class UsfmRem(Usfm):
+    # \rem text, a comment
+    def __init__(self, Marker, Content):
+        super().__init__(Marker, "", Content)
+    def print(self, f=sys.stdout):
+        print(f'{self.marker} {self.content}', file=f)
 
 class Book:
     # The layout of USFM is fairly flat, with a chapter mainly consisting
@@ -174,7 +201,7 @@ class Book:
                 u = UsfmV(marker, words.pop(0), str(' '.join(words)))
             elif (marker == "\\c"):
                 u = UsfmC(marker, words.pop(0))
-            elif (marker == "\\p"):
+            elif (marker == "\\p" or marker == "\\ip"):
                 u = UsfmP(marker, str(' '.join(words)))
             elif (marker == "\\s" or marker == "\\s1" or marker == "\\s2"):
                 u = UsfmS(marker, str(' '.join(words)))
@@ -182,7 +209,7 @@ class Book:
                 u = UsfmH(marker, str(' '.join(words)))
             elif (marker == "\\toc1" or marker == "\\toc2" or marker == "\\toc3"):
                 u = UsfmTOC(marker, str(' '.join(words)))
-            elif (marker == "\\mt" or marker == "\\mt1" or marker == "\\mt2" or marker == "\\mt3" or marker == "\\ms"):
+            elif (marker == "\\mt" or marker == "\\mt1" or marker == "\\mt2" or marker == "\\mt3" or marker == "\\ms" or marker == "\\imt1"):
                 u = UsfmMT(marker, str(' '.join(words)))
             elif (marker == "\\id"):
                 # \id MAT <other text may appear here but probably should not>
@@ -190,7 +217,7 @@ class Book:
                 u = UsfmId(marker, self.id, str(' '.join(words)))
                 #print("New ID Marker: ", end="")
                 #u.print()
-            elif (marker == "\\q1"):
+            elif (marker == "\\q1" or marker == "\\q2" or marker == "\\q3" or marker == "\\q4"):
                 u = UsfmQ(marker, str(' '.join(words)))
             elif (marker == "\\m"):
                 u = UsfmM(marker, str(' '.join(words)))
@@ -198,10 +225,20 @@ class Book:
                 u = UsfmR(marker, str(' '.join(words)))
             elif (marker == "\\mr"):
                 u = UsfmR(marker, str(' '.join(words)))
+            elif (marker == "\\rq"):
+                if (words[-1] == '\\rq*'):
+                    words.pop()
+                else:
+                    print("ERROR: Did not find final \\rq* endmarker")
+                u = UsfmRq(marker, str(' '.join(words)))
+            elif (marker == "\\li1"):
+                u = UsfmLi(marker, str(' '.join(words)))
+            elif (marker == "\\rem"):
+                u = UsfmRem(marker, str(' '.join(words)))
             elif (marker[0] == "\\"):
                 print("New UNKNOWN Marker: ", marker)
                 # There is some other marker here, but I don't specifically care what it is
-                self.arg = words.pop(0)
+                self.arg = words.pop(0) # this assumes it has an argument. It might not (blank \ip lines, which should not exist, but...)
                 u = Usfm(marker, self.arg, str(' '.join(words)))
                 #u.print()
             else:
@@ -306,11 +343,35 @@ class Book:
         # Now update the final USFM
         self.usfms = newu
 
-    def print(self, FileName):
-        #file = open(FileName,'w')
-        # use FileName later
+    def fixSubHeads(self):
+        # This originated with the Kabiye project when we learned that it was
+        # making the same mistake we had made elsewhere--putting \s1 before \c
+        # which has to be swapped.
+        # Run this AFTER combineLines has combined everything
+        newu = [] # Since we have to swap lines, just build a new list of usfms
+        for idx, u in enumerate(self.usfms):
+            #print("Processing USFM >> ", end="")
+            #u.print()
+            if (isinstance(u, UsfmC)):
+                # Check if preceding was a \s#. If so, print message and put this one
+                # BEFORE that \s# line.
+                if (isinstance(newu[-1], UsfmS)):
+                    #print(f"line {idx+1}: \\c after \\s# in {u}")
+                    sMarkeru = newu.pop()  # pop off the \s marker line
+                    newu.append(u)         # push on the \c marker
+                    u = sMarkeru           # prepare to push on the \s marker next
+                # Append either the \c marker or the swapped out \s# marker from the if stmt
+                newu.append(u)
+            else:
+                # Anything else just is just forwarded
+                newu.append(u)
+
+        # Now update the final USFM
+        self.usfms = newu
+
+    def print(self, f=sys.stdout):
         for u in self.usfms:
-            u.print()
+            u.print(f)
 
 class Bible:
     def __init__(self, BibleName):
@@ -318,14 +379,14 @@ class Bible:
         # Bible contains a list of books
         self.books = []
 
-    def printInternals(self):
-        print(f'Data for {self.name}')
+    def printInternals(self, f=sys.stdout):
+        print(f'Data for {self.name}', file=f)
         for book in self.books:
-            print(type(book))
+            print(type(book), file=f)
             book.printInternals()
 
-    def printname(self):
-        print(self.name)
+    def printname(self, f=sys.stdout):
+        print(self.name, file=f)
 
     def load(self):
         pass
@@ -338,9 +399,9 @@ class Bible:
     def lastBook(self):
         return self.books[-1]
 
-    def print(self, FileName):
+    def print(self, f=sys.stdout):
         for book in self.books:
-            book.print(FileName)
+            book.print(f)
 
 def main():
     print("This is the USFM library; you cannot run it on its own!")
