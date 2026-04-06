44e81f818bc462925f6985bb93f5a48d4c46ef66
Matt Postiff <postiffm@umich.edu>
2026-04-06 16:23:21 -0400

    Add evalRequirements.txt for running evalAIDraft.py

--- evalAIDraft.py
+++ evalAIDraft.py
@@ -2,6 +2,17 @@ import os
 import subprocess
 import sys
 
+ # How to Set up environment
+# First time:
+# python3 -m venv .ubuvenv or .wslvenv or .deskvenv
+# pip3 install -r evalRequirements.txt
+# On Ubuntu:
+# source .ubuvenv/bin/activate
+# On Windows Laptop:
+# source .wslvenv/bin/activate
+# pip3 install evaluate sacrebleu levenshtein
+# Thereafter, just do the source command appropriate for the platform.
+
 # Usage:
 # Update the files list below with the files you want to process
 # python3 evalAIDraft.py
@@ -15,6 +26,45 @@ LEV = f"{USFMTOOLS}/levenshteinFiles.py"
 files = [
 ("08RUTCSV.SFM", "rut_bsb_falam_04042026.usfm"),
 ("32JONCSV.SFM", "jon_bsb_falam_04052026.usfm"),
+    ("01GENCSV.SFM", ""),
+    ("02EXOCSV.SFM", ""),
+    ("03LEVCSV.SFM", ""),
+    ("04NUMCSV.SFM", ""),
+    ("05DEUCSV.SFM", ""),
+    ("06JOSCSV.SFM", ""),
+    ("07JDGCSV.SFM", ""),
+    ("08RUTCSV.SFM", ""),
+    ("091SACSV.SFM", ""),
+    ("102SACSV.SFM", ""),
+    ("111KICSV.SFM", ""),
+    ("122KICSV.SFM", ""),
+    ("131CHCSV.SFM", ""),
+    ("142CHCSV.SFM", ""),
+    ("15EZRCSV.SFM", ""),
+    ("16NEHCSV.SFM", ""),
+    ("17ESTCSV.SFM", ""),
+    ("18JOBCSV.SFM", ""),
+    ("19PSACSV.SFM", ""),
+    ("20PROCSV.SFM", ""),
+    ("21ECCCSV.SFM", ""),
+    ("22SNGCSV.SFM", ""),
+    ("23ISACSV.SFM", ""),
+    ("24JERCSV.SFM", ""),
+    ("25LAMCSV.SFM", ""),
+    ("26EZKCSV.SFM", ""),
+    ("27DANCSV.SFM", ""),
+    ("28HOSCSV.SFM", ""),
+    ("29JOLCSV.SFM", ""),
+    ("30AMOCSV.SFM", ""),
+    ("31OBACSV.SFM", ""),
+    ("32JONCSV.SFM", ""),
+    ("33MICCSV.SFM", ""),
+    ("34NAMCSV.SFM", ""),
+    ("35HABCSV.SFM", ""),
+    ("36ZEPCSV.SFM", ""),
+    ("37HAGCSV.SFM", ""),
+    ("38ZECCSV.SFM", ""),
+    ("39MALCSV.SFM", ""),
 ]
 
 folder = sys.argv[1] if len(sys.argv) > 1 else "."
@@ -38,4 +88,3 @@ for goodF, aiF in files:
     with open(aiAcc, 'w') as f:
         subprocess.run(["python3", USFMtoACC, "--no-para", aiF], stdout=f)     # ai usfm > acc
     subprocess.run(["python3", LEV, goodAcc, aiAcc])
-
